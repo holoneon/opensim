b64773a1ce57780c26e72b63d2ae1405b1853253
Fiona Sweet <fiona@pobox.holoneon.com>
2026-09-02 12:25:47 +0200

    update friend status remove break that bails after first friend

--- OpenSim/Services/HypergridService/HGFriendsService.cs
+++ OpenSim/Services/HypergridService/HGFriendsService.cs
@@ -254,27 +254,38 @@ namespace OpenSim.Services.HypergridService
             //m_log.DebugFormat("[HGFRIENDS SERVICE]: Status notification: user has {0} local friends", usersToBeNotified.Count);
 
             // First, let's send notifications to local users who are online in the home grid
-            PresenceInfo[] friendSessions = m_PresenceService.GetAgents(usersToBeNotified.ToArray());
+            //  update by Fiona Sweet - the break causes the function to bail after the first friend (friend_0) 
+
+            PresenceInfo[] friendSessions =
+                m_PresenceService.GetAgents(usersToBeNotified.ToArray());
+            
             if (friendSessions != null && friendSessions.Length > 0)
             {
-                PresenceInfo friendSession = null;
-                foreach (PresenceInfo pinfo in friendSessions)
-                    if (!pinfo.RegionID.IsZero()) // let's guard against traveling agents
-                    {
-                        friendSession = pinfo;
-                        break;
-                    }
-
-                if (friendSession != null)
+                foreach (PresenceInfo friendSession in friendSessions)
                 {
-                    ForwardStatusNotificationToSim(friendSession.RegionID, foreignUserID, friendSession.UserID, online);
-                    usersToBeNotified.Remove(friendSession.UserID.ToString());
-                    UUID id;
-                    if (UUID.TryParse(friendSession.UserID, out id))
+                    // Guard against agents currently traveling between regions.
+                    if (friendSession.RegionID.IsZero())
+                        continue;
+            
+                    ForwardStatusNotificationToSim(
+                        friendSession.RegionID,
+                        foreignUserID,
+                        friendSession.UserID,
+                        online
+                    );
+            
+                    usersToBeNotified.Remove(
+                        friendSession.UserID.ToString()
+                    );
+            
+                    if (UUID.TryParse(friendSession.UserID, out UUID id) &&
+                        !localFriendsOnline.Contains(id))
+                    {
                         localFriendsOnline.Add(id);
-
+                    }
                 }
             }
+
 
 //            // Lastly, let's notify the rest who may be online somewhere else
 //            foreach (string user in usersToBeNotified)
